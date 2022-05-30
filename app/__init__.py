e4803a890d08c4fb2c053529f5d3ed733ad7d3ec
yoondoryee <younseoj00@naver.com>
2022-05-31 02:24:04 +0900

    404 Error add

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
@@ -13,6 +13,9 @@ naming_convention = {
 }
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
+
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 def create_app():
 	app = Flask(__name__)
@@ -39,5 +42,9 @@ def create_app():
 	
 	# markdown
 	Markdown(app, extensions=['nl2br', 'fenced_code'])
+	
+	 # Error page
+	app.register_error_handler(404, page_not_found)
+
     
 	return app
