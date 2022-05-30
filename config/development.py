e9b60e9a3d779673000cb1684c6effc3bc8b380d
yoondoryee <59691428+yoondoryee@users.noreply.github.com>
2022-05-31 01:23:02 +0900

    Update development.py

--- config/development.py
+++ config/development.py
@@ -1,9 +1,6 @@
 from config.default import *
 
-<<<<<<< HEAD
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'app.db'))
-=======
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
->>>>>>> 3ce3c922b9ba1a78fc37939f54bc7212dd3969a8
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "dev"
